f0af171d5b5891e8a0bb29e3233e3ad4741cb7e9
watrabi <itswaterboi@gmail.com>
2025-10-24 22:36:15 +0000

    improve the pages and blob

--- public/assets/js/watrlabs.js
+++ public/assets/js/watrlabs.js
@@ -3,14 +3,24 @@
 
 const sidebar = document.getElementById("sidebar");
 const sidebartoggle = document.getElementById("sidebar-nav");
+const mainelement = document.getElementById("main");
+const header = document.getElementById("header");
+const footer = document.getElementById("footer");
+
 ishidden = sidebar.classList.contains('side-hidden');
 
 function toggleSideBar(){
     if(ishidden){
         sidebar.classList.remove("side-hidden");
+        mainelement.style.paddingLeft = "250px";
+        footer.style.paddingLeft = "250px";
+        header.style.paddingLeft = "250px";
         ishidden = sidebar.classList.contains('side-hidden'); // redo it so the user cant break it
     } else {
-        sidebar.classList.add("side-hidden");        
+        sidebar.classList.add("side-hidden");
+        mainelement.style.paddingLeft = "0px";
+        footer.style.paddingLeft = "0px";
+        header.style.paddingLeft = "0px";
         ishidden = sidebar.classList.contains('side-hidden'); 
     }
     
